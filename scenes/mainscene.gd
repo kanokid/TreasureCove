36da2e474e14535f3c7f9acd7eeaf4cf084f0233
kanokid <57643139+kanokid@users.noreply.github.com>
2026-04-10 22:23:50 -0400

    messed around with coins and changed project name

--- scenes/mainscene.gd
+++ scenes/mainscene.gd
@@ -3,15 +3,17 @@ extends Node
 const coin_node = preload("res://scenes/coin.tscn")
 
 const coins_to_create = [
-   Vector2(0, 0),
+   Vector2(225, 0),
    Vector2(100, 200),
    Vector2(400, 300),
    Vector2(123, 456),
-   Vector2(456, 789),
+   Vector2(456, -100),
    Vector2(-100, -200),
-   Vector2(350, 800),
-   Vector2(30, 800),
-   Vector2(-25, 700),
+   Vector2(350, -350),
+   Vector2(30, -175),
+   Vector2(-25, 0),
+   Vector2(-60,400),
+   #Vector2(500,0),
 	]
 # add a bunch of places to create coins at
 		
